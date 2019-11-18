8da897a0e727172ee45f9b55f30bf643a1efb5db
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-18 16:28:27 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,8 +16,9 @@ def hopper
         }
      }
 
-
+programmer_hash [:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay ][:known_for]   
 end
 
 def dennis_ritchies_language
@@ -55,8 +58,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash [:dennis_ritchie][:languages].first  #i keep getting "C" with brackets how do i stop the brackets ?    
+     
 end
-
+ 
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -66,6 +71,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -100,9 +109,15 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+    
+   alans_new_info = "GUI"
      
+   programmer_hash[:alan_kay][:known_for] = alans_new_info 
+ 
+ return programmer_hash
      
+ 
+    
 end
 
 def adding_to_dennis
@@ -121,5 +136,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     
+     
+     programmer_hash[:dennis_ritchie][:languages][1]  = "Assembly"
 
+return programmer_hash
 end
