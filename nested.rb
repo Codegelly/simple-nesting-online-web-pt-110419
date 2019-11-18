a2def98c6a94debbce2d3fe8342f42686baceb44
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-18 16:07:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,6 +137,6 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash = [:dennis_ritchie][:languages].include "Assembly"
+     programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
 
 end
